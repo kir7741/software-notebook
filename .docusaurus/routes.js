a61690d5075d9810557d791f73129ec897b6fec6
kir7741 <typhoonjoe2003@yahoo.com.tw>
2024-05-08 16:33:43 +0800

    feat: update config files

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,175 +3,300 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '60c'),
+    path: '/software-notebook/',
+    component: ComponentCreator('/software-notebook/', '038'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '0d6'),
+    path: '/software-notebook/__docusaurus/debug',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug', 'b8f'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'efe'),
+    path: '/software-notebook/__docusaurus/debug/config',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/config', '83f'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', '878'),
+    path: '/software-notebook/__docusaurus/debug/content',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/content', '9b5'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '633'),
+    path: '/software-notebook/__docusaurus/debug/globalData',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/globalData', '942'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '256'),
+    path: '/software-notebook/__docusaurus/debug/metadata',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/metadata', '453'),
     exact: true
   },
   {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '34f'),
+    path: '/software-notebook/__docusaurus/debug/registry',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/registry', 'bd2'),
     exact: true
   },
   {
-    path: '/blog',
-    component: ComponentCreator('/blog', 'c3e'),
+    path: '/software-notebook/__docusaurus/debug/routes',
+    component: ComponentCreator('/software-notebook/__docusaurus/debug/routes', '594'),
     exact: true
   },
   {
-    path: '/blog/archive',
-    component: ComponentCreator('/blog/archive', '0be'),
+    path: '/software-notebook/blog',
+    component: ComponentCreator('/software-notebook/blog', 'da9'),
     exact: true
   },
   {
-    path: '/blog/first-blog-post',
-    component: ComponentCreator('/blog/first-blog-post', 'f0e'),
+    path: '/software-notebook/blog/archive',
+    component: ComponentCreator('/software-notebook/blog/archive', 'ce9'),
     exact: true
   },
   {
-    path: '/blog/long-blog-post',
-    component: ComponentCreator('/blog/long-blog-post', 'c3b'),
+    path: '/software-notebook/blog/first-blog-post',
+    component: ComponentCreator('/software-notebook/blog/first-blog-post', '1ab'),
     exact: true
   },
   {
-    path: '/blog/mdx-blog-post',
-    component: ComponentCreator('/blog/mdx-blog-post', 'dc4'),
+    path: '/software-notebook/blog/long-blog-post',
+    component: ComponentCreator('/software-notebook/blog/long-blog-post', 'd2d'),
     exact: true
   },
   {
-    path: '/blog/tags',
-    component: ComponentCreator('/blog/tags', '20d'),
+    path: '/software-notebook/blog/mdx-blog-post',
+    component: ComponentCreator('/software-notebook/blog/mdx-blog-post', '0fa'),
     exact: true
   },
   {
-    path: '/blog/tags/docusaurus',
-    component: ComponentCreator('/blog/tags/docusaurus', '274'),
+    path: '/software-notebook/blog/tags',
+    component: ComponentCreator('/software-notebook/blog/tags', 'e8a'),
     exact: true
   },
   {
-    path: '/blog/tags/facebook',
-    component: ComponentCreator('/blog/tags/facebook', 'b76'),
+    path: '/software-notebook/blog/tags/docusaurus',
+    component: ComponentCreator('/software-notebook/blog/tags/docusaurus', '8aa'),
     exact: true
   },
   {
-    path: '/blog/tags/hello',
-    component: ComponentCreator('/blog/tags/hello', '22a'),
+    path: '/software-notebook/blog/tags/facebook',
+    component: ComponentCreator('/software-notebook/blog/tags/facebook', 'cad'),
     exact: true
   },
   {
-    path: '/blog/tags/hola',
-    component: ComponentCreator('/blog/tags/hola', '570'),
+    path: '/software-notebook/blog/tags/hello',
+    component: ComponentCreator('/software-notebook/blog/tags/hello', '2b4'),
     exact: true
   },
   {
-    path: '/blog/welcome',
-    component: ComponentCreator('/blog/welcome', '65c'),
+    path: '/software-notebook/blog/tags/hola',
+    component: ComponentCreator('/software-notebook/blog/tags/hola', '6a7'),
     exact: true
   },
   {
-    path: '/markdown-page',
-    component: ComponentCreator('/markdown-page', '8f3'),
+    path: '/software-notebook/blog/welcome',
+    component: ComponentCreator('/software-notebook/blog/welcome', 'd23'),
     exact: true
   },
   {
-    path: '/docs',
-    component: ComponentCreator('/docs', '4ff'),
+    path: '/software-notebook/markdown-page',
+    component: ComponentCreator('/software-notebook/markdown-page', 'e24'),
+    exact: true
+  },
+  {
+    path: '/software-notebook/docs',
+    component: ComponentCreator('/software-notebook/docs', '987'),
     routes: [
       {
-        path: '/docs',
-        component: ComponentCreator('/docs', '138'),
+        path: '/software-notebook/docs',
+        component: ComponentCreator('/software-notebook/docs', '576'),
         routes: [
           {
-            path: '/docs',
-            component: ComponentCreator('/docs', '0c0'),
+            path: '/software-notebook/docs/tags',
+            component: ComponentCreator('/software-notebook/docs/tags', '156'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/css-reflow',
+            component: ComponentCreator('/software-notebook/docs/tags/css-reflow', 'da5'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/database',
+            component: ComponentCreator('/software-notebook/docs/tags/database', '016'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/design-pattern',
+            component: ComponentCreator('/software-notebook/docs/tags/design-pattern', '5b2'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/dns',
+            component: ComponentCreator('/software-notebook/docs/tags/dns', '1f5'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/dotenv',
+            component: ComponentCreator('/software-notebook/docs/tags/dotenv', '8a7'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/env',
+            component: ComponentCreator('/software-notebook/docs/tags/env', 'e44'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/godotenv',
+            component: ComponentCreator('/software-notebook/docs/tags/godotenv', 'a2d'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/gorm',
+            component: ComponentCreator('/software-notebook/docs/tags/gorm', 'd97'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/html',
+            component: ComponentCreator('/software-notebook/docs/tags/html', '545'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/mvc',
+            component: ComponentCreator('/software-notebook/docs/tags/mvc', '203'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs/tags/mvvm',
+            component: ComponentCreator('/software-notebook/docs/tags/mvvm', '5d1'),
+            exact: true
+          },
+          {
+            path: '/software-notebook/docs',
+            component: ComponentCreator('/software-notebook/docs', 'bb5'),
             routes: [
               {
-                path: '/docs/category/tutorial---basics',
-                component: ComponentCreator('/docs/category/tutorial---basics', 'd44'),
+                path: '/software-notebook/docs/category/database',
+                component: ComponentCreator('/software-notebook/docs/category/database', '109'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/category/tutorial---extras',
-                component: ComponentCreator('/docs/category/tutorial---extras', 'f09'),
+                path: '/software-notebook/docs/category/golang',
+                component: ComponentCreator('/software-notebook/docs/category/golang', 'f07'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/intro',
-                component: ComponentCreator('/docs/intro', '61d'),
+                path: '/software-notebook/docs/category/gorm',
+                component: ComponentCreator('/software-notebook/docs/category/gorm', 'ebb'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/congratulations',
-                component: ComponentCreator('/docs/tutorial-basics/congratulations', '458'),
+                path: '/software-notebook/docs/category/postgresql',
+                component: ComponentCreator('/software-notebook/docs/category/postgresql', '64c'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-blog-post',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-blog-post', '108'),
+                path: '/software-notebook/docs/category/program-concepts',
+                component: ComponentCreator('/software-notebook/docs/category/program-concepts', '8cb'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-document',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-document', '8fc'),
+                path: '/software-notebook/docs/category/tutorial---basics',
+                component: ComponentCreator('/software-notebook/docs/category/tutorial---basics', 'efe'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-page',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-page', '951'),
+                path: '/software-notebook/docs/category/tutorial---extras',
+                component: ComponentCreator('/software-notebook/docs/category/tutorial---extras', '0e5'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/deploy-your-site',
-                component: ComponentCreator('/docs/tutorial-basics/deploy-your-site', '4f5'),
+                path: '/software-notebook/docs/database/postgresql/postgresql-install',
+                component: ComponentCreator('/software-notebook/docs/database/postgresql/postgresql-install', '7f8'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/markdown-features',
-                component: ComponentCreator('/docs/tutorial-basics/markdown-features', 'b05'),
+                path: '/software-notebook/docs/golang/environment-variable',
+                component: ComponentCreator('/software-notebook/docs/golang/environment-variable', '835'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-extras/manage-docs-versions',
-                component: ComponentCreator('/docs/tutorial-extras/manage-docs-versions', '978'),
+                path: '/software-notebook/docs/golang/gorm/gorm-install',
+                component: ComponentCreator('/software-notebook/docs/golang/gorm/gorm-install', '268'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-extras/translate-your-site',
-                component: ComponentCreator('/docs/tutorial-extras/translate-your-site', 'f9a'),
+                path: '/software-notebook/docs/intro',
+                component: ComponentCreator('/software-notebook/docs/intro', 'bd1'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/program-concept/browser-render-flow',
+                component: ComponentCreator('/software-notebook/docs/program-concept/browser-render-flow', '028'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/program-concept/mvc-vs-mvp',
+                component: ComponentCreator('/software-notebook/docs/program-concept/mvc-vs-mvp', 'cbd'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/congratulations',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/congratulations', 'c61'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/create-a-blog-post',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/create-a-blog-post', 'c7e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/create-a-document',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/create-a-document', '99d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/create-a-page',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/create-a-page', '2b0'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/deploy-your-site',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/deploy-your-site', '22e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-basics/markdown-features',
+                component: ComponentCreator('/software-notebook/docs/tutorial-basics/markdown-features', '29b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-extras/manage-docs-versions',
+                component: ComponentCreator('/software-notebook/docs/tutorial-extras/manage-docs-versions', '0ce'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/software-notebook/docs/tutorial-extras/translate-your-site',
+                component: ComponentCreator('/software-notebook/docs/tutorial-extras/translate-your-site', '8e9'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
@@ -180,11 +305,6 @@ export default [
         ]
       }
     ]
-  },
-  {
-    path: '/',
-    component: ComponentCreator('/', '01f'),
-    exact: true
   },
   {
     path: '*',
